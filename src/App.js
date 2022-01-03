4e694b3fedda405c8aad5a81ee3a70ed89d12bf2
Matias Garcia <matiasigarcia91@gmail.com>
2022-01-03 14:54:38 +0100

    Add page skeleton

--- src/App.js
+++ src/App.js
@@ -8,21 +8,12 @@ import MessageBox from "./components/MessageBox";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 
+import { Jumbotron } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
+import Homepage from "./pages/Home";
+import Space from "./pages/Space";
 import { selectAppLoading } from "./store/appState/selectors";
 import { getUserWithStoredToken } from "./store/user/actions";
-import { Jumbotron } from "react-bootstrap";
-
-const Home = () => (
-  <Jumbotron>
-    <h1>Home</h1>
-  </Jumbotron>
-);
-const Other = () => (
-  <Jumbotron>
-    <h1>Other</h1>
-  </Jumbotron>
-);
 
 function App() {
   const dispatch = useDispatch();
@@ -33,15 +24,15 @@ function App() {
   }, [dispatch]);
 
   return (
-    <div className="App">
+    <div className='App'>
       <Navigation />
       <MessageBox />
       {isLoading ? <Loading /> : null}
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/other" component={Other} />
-        <Route path="/signup" component={SignUp} />
-        <Route path="/login" component={Login} />
+        <Route exact path='/' component={Homepage} />
+        <Route path='/space/:id' component={Space} />
+        <Route path='/signup' component={SignUp} />
+        <Route path='/login' component={Login} />
       </Switch>
     </div>
   );
---
+++ src/pages/Home/index.js
@@ -0,0 +1,9 @@
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Home page :D</h1>
+    </div>
+  );
+};
+
+export default Homepage;
